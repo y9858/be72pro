1ec6e2e965134e0eaba392ac99d665b2ad721ff6
y9858 <53130835+y9858@users.noreply.github.com>
2026-03-28 13:54:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -36,7 +36,7 @@ git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-momo package/OpenW
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
-git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall2 package/openwrt-passwall2
+git clone --depth 1 https://github.com/y9858/openwrt-passwall2 package/openwrt-passwall2
 
 # 修改首页显示
 rm -rf feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/40_dhcp.js
