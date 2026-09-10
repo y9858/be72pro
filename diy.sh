c2dce768519f24766ded9ac720efb2fa09521ded
y9858 <53130835+y9858@users.noreply.github.com>
2026-09-10 11:14:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -54,3 +54,5 @@ git clone --depth 1 https://github.com/sbwml/packages_lang_rust feeds/packages/l
 rm -rf package/mtk/applications/luci-app-turboacc-mtk/po/zh_Hans/turboacc.po
 curl -o package/mtk/applications/luci-app-turboacc-mtk/po/zh_Hans/turboacc.po https://raw.githubusercontent.com/y9858/Home-mod/refs/heads/main/turboacc.po
 git clone --depth 1 https://github.com/RuijieNetworksCommunity/rtl837x-dsa-driver package/rtl837x-dsa-driver
+sed -i 's/2.43.0/2.63.23/g' feeds/packages/utils/filebrowser/Makefile
+sed -i 's/9c3acee7567986ceef331e3eea0236b4dd9a41bf3874449fbb9bdc22c00a0aab/18020983f6a4e43d679738738c3f95c52a333a282144046ec152491f2341a152/g' feeds/packages/utils/filebrowser/Makefile
